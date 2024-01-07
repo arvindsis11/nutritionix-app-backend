86e47b9b58ae70b43dc38644f69db183f58cc208
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2024-01-07 15:39:30 +0530

    initial skeleton for whishlist service

---
+++ whishlist-service/src/test/java/com/nutritionix/whishlist/WhishlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritionix.whishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhishlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
