1b641c58e88317ffbc6058ec7dd564a77c266a4f
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2024-01-07 17:43:43 +0530

    completed api gateway

--- nutritionix-service/src/main/java/com/nutritionix/app/controller/NutritionixController.java
+++ nutritionix-service/src/main/java/com/nutritionix/app/controller/NutritionixController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nutritionix.app.service.NutritionixService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
 @RequestMapping("/api/v1.0/nutrition")
@@ -25,14 +27,14 @@ public class NutritionixController {
 	
 	@GetMapping("/foodItem/{query}")
 	@Operation(summary = "get common food items by name")
-	public ResponseEntity<?> getCommonFoodItems(@PathVariable String query){
+	public ResponseEntity<?> getCommonFoodItems(@PathVariable String query,@Parameter(hidden = true) @RequestHeader("Authorization") String token){
 		
 		return nutritionixService.getCommonFoodItems(query);	
 	}
 	
 	@GetMapping("/getNutrients")
 	@Operation(summary = "get common food nutrients by name")
-	public ResponseEntity<?> getFoodNutritions(@RequestParam String foodName){
+	public ResponseEntity<?> getFoodNutritions(@RequestParam String foodName,@Parameter(hidden = true) @RequestHeader("Authorization") String token){
 		
 		return nutritionixService.getFoodNutritions(foodName);	
 	}
