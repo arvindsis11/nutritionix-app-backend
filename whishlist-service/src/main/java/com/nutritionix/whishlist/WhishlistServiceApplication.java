a45865a7153ea17930660024383d2f433b54870f
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2024-01-07 15:53:15 +0530

    eureka registry and fein enabled

--- whishlist-service/src/main/java/com/nutritionix/whishlist/WhishlistServiceApplication.java
+++ whishlist-service/src/main/java/com/nutritionix/whishlist/WhishlistServiceApplication.java
@@ -2,8 +2,12 @@ package com.nutritionix.whishlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class WhishlistServiceApplication {
 
 	public static void main(String[] args) {
