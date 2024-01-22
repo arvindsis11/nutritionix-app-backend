8a5a58c536aef2319c5a7fde3bd16523c09cdd4a
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2024-01-22 22:08:09 +0530

    added exception handling and jwt authorization

--- nutritionix-service/src/main/java/com/nutritionix/app/controller/NutritionixController.java
+++ nutritionix-service/src/main/java/com/nutritionix/app/controller/NutritionixController.java
@@ -1,5 +1,7 @@
 package com.nutritionix.app.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nutritionix.app.exception.ExternalServiceException;
+import com.nutritionix.app.exception.InvalidCredentialsException;
+import com.nutritionix.app.feign.AuthClient;
 import com.nutritionix.app.service.NutritionixService;
 
+import feign.FeignException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
@@ -21,23 +27,43 @@ public class NutritionixController {
 
 	private final NutritionixService nutritionixService;
 
-	public NutritionixController(NutritionixService nutritionixService) {
+	private final AuthClient authClient;
+
+	public NutritionixController(NutritionixService nutritionixService, AuthClient authClient) {
 		this.nutritionixService = nutritionixService;
+		this.authClient = authClient;
 	}
-	
+
 	@GetMapping("/foodItem/{query}")
 	@Operation(summary = "get common food items by name")
-	public ResponseEntity<?> getCommonFoodItems(@PathVariable String query,@Parameter(hidden = true) @RequestHeader("Authorization") String token){
-		
-		return nutritionixService.getCommonFoodItems(query);	
+	public ResponseEntity<?> getCommonFoodItems(@PathVariable String query,
+			@Parameter(hidden = true) @RequestHeader("Authorization") String token) {
+		try {
+			Map<String, String> userInfo = (Map<String, String>) authClient.validateToken(token).getBody();
+			if (userInfo.containsValue("ROLE_ADMIN") || userInfo.containsValue("ROLE_CUSTOMER")) {
+				return nutritionixService.getCommonFoodItems(query);
+			} else {
+				throw new InvalidCredentialsException("Access Denied");
+			}
+		} catch (FeignException e) {
+			throw new ExternalServiceException(e.getMessage());
+		}
 	}
-	
+
 	@GetMapping("/getNutrients")
 	@Operation(summary = "get common food nutrients by name")
-	public ResponseEntity<?> getFoodNutritions(@RequestParam String foodName,@Parameter(hidden = true) @RequestHeader("Authorization") String token){
-		
-		return nutritionixService.getFoodNutritions(foodName);	
+	public ResponseEntity<?> getFoodNutritions(@RequestParam String foodName,
+			@Parameter(hidden = true) @RequestHeader("Authorization") String token) {
+		try {
+			Map<String, String> userInfo = (Map<String, String>) authClient.validateToken(token).getBody();
+			if (userInfo.containsValue("ROLE_ADMIN") || userInfo.containsValue("ROLE_CUSTOMER")) {
+				return nutritionixService.getFoodNutritions(foodName);
+			} else {
+				throw new InvalidCredentialsException("Access Denied");
+			}
+		} catch (FeignException e) {
+			throw new ExternalServiceException(e.getMessage());
+		}
 	}
-	
-	
+
 }
